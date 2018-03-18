2be30ddd2eaf373d863af1b5d7f900019d50c6ba
sofiamv <League@AdministorsiMac.attlocal.net>
2018-03-18 11:26:48 -0700

    Sorry no time for names

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,20 +8,20 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
-
+Robot r2d2 = new Robot();
 		//2. Set the speed to 100
-
+r2d2.setSpeed(100);
 		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
+		r2d2.setPenColor(colorChoice);
 		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
+		String Polygons = JOptionPane.showInputDialog("How many polygons do you want?");
+	int polygons =	Integer.parseInt(Polygons);
 		//6. Make it so your shapes do not overlap
-
+	r2d2.hide();
+r2d2.penDown();
+r2d2.move(100);
 		//7. Challenge: add more colors to the Option Dialog.
 	}
 }
-
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -10,26 +11,30 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String Name = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		System.out.println(Name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < Name.length(); i++) {
 			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			// using .substring(start, end)
+			// HINT: replace 'start' and 'end' to get String with character at i
+String Letter = Name.substring(i, i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
-		// 7. Use pop-up to show user their Goofy name
-
+			// and lower case for ODD characters.
+if (i%2==0) {
+	System.out.println(Letter.toUpperCase());
+	goofyName += Letter.toUpperCase();
+}
+if (i%2==1) {
+	System.out.println(Letter.toLowerCase());
+	goofyName += Letter.toLowerCase();
+}
+			// 6. ADD this String (containing 1 char) to the goofyName String
+			// 7. Use pop-up to show user their Goofy name
+		}
+		JOptionPane.showMessageDialog(null, "Your goofy name is " + goofyName + ".");
 	}
 }
-
